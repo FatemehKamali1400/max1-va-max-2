e8df5e2903fd09e75d374dacc35658c1336c3510
Yamahdi@1984 <fatemeh.kamali198422@gmail.com>
2022-07-09 02:22:49 +0430

    tamrin2

---
+++ max1 va max 2/Program.cs
@@ -0,0 +1,25 @@
+﻿int max1=0;
+int max2=0;
+
+for (int i = 0; i < 10; i++)
+{
+    System.Console.WriteLine("please Enter num:");
+    int num=int.Parse( Console.ReadLine());
+   if (num>=max1)
+   {
+   max2=max1;
+   max1=num;
+   }
+
+   else{
+    if (num>=max2)
+    {
+        max2=num;
+    }
+  }
+}
+
+System.Console.WriteLine( "max1= " + max1 + " " + "max2= " + max2 );
+
+
+
